b6794476eb025767dd9dcb7ae7c978b561a38ee2
Bojan Markovic <bmarkovic.79@gmail.com>
2014-11-05 22:11:33 +0100

    PouchDB Cloudant Sync try 1

--- js/main.js
+++ js/main.js
@@ -1,23 +1,60 @@
-var app = {
+// var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
+//     findByName: function() {
+//         console.log('findByName');
+//         this.store.findByName($('.search-key').val(), function(employees) {
+//             var l = employees.length;
+//             var e;
+//             $('.employee-list').empty();
+//             for (var i=0; i<l; i++) {
+//                 e = employees[i];
+//                 $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+//             }
+//         });
+//     },
 
-    initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+//     showAlert: function (message, title) {
+//         if (navigator.notification) {
+//             navigator.notification.alert(message, null, title, 'OK');
+//         } else {
+//             alert(title ? (title + ": " + message) : message);
+//         }
+//     },
 
-};
+//     initialize: function() {
+//         this.store = new MemoryStore(function() {
+//             self.showAlert('Store Initialized', 'Info');
+//             });
 
-app.initialize();
+//         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+//     }
+
+// };
+
+var remoteDB = {
+    user: 'thoushoonlyconceencither',
+    pass: 'f6XftcvPtpb6hGT1YyRRS2hI',
+    server: 'elivero.cloudant.com',
+    db: 'pouch'
+}
+
+var db = new PouchDB('pouch');
+
+db.put({
+  _id: 'cabbage',
+  image: 'cabbage.gif',
+  price: 3
+});
+
+db.changes().on('change', function() {
+  console.log('Ch-Ch-Changes');
+});
+
+remoteDB.dsn = 'http://' + remoteDB.user + ':' + remoteDB.pass + '@' +
+               remoteDB.server + '/' + remoteDB.db;
+
+console.log("Remote DSN: ", remoteDB.dsn);
+
+db.replicate.to(remoteDB.dsn);
+
+// app.initialize();
---
+++ log1.js
@@ -0,0 +1,36 @@
+Remote DSN:  http://thoushoonlyconceencither:f6XftcvPtpb6hGT1YyRRS2hI@elivero.cloudant.com/pouch main.js:56
+GET blob:null/62b78ffd-b653-4624-8c99-fd5dd66328ae 404 (Not Found) pouchdb-3.0.6.min.js:8
+Ch-Ch-Changes main.js:50
+GET http://elivero.cloudant.com/?_nonce=yoiH75EvEl6fSDuG 404 (Object Not Found) elivero.cloudant.com/?_nonce=yoiH75EvEl6fSDuG:1
+db.sync('pouch', remoteDB.dsn);
+r {canceled: false, push: r, pull: r, _events: Object, then: function…}_events: Objectcanceled: truecatch: function (e){return m["catch"](e)}pull: rpush: rthen: function (e,t){return m.then(e,t)}__proto__: r
+db._allDocs;
+function (e,t){y(function(n,r){return n?t(n):0===e.limit?t(null,{total_rows:r,offset:e.skip,rows:[]}):void m(r,e,t)})} pouchdb-3.0.6.min.js:7
+console.log(db._al
+SyntaxError: Unexpected token }
+console.log(db._allDocs);
+function (e,t){y(function(n,r){return n?t(n):0===e.limit?t(null,{total_rows:r,offset:e.skip,rows:[]}):void m(r,e,t)})} VM230:2
+undefined
+db._allDocs(function (err,response){console.log(response)});
+undefined
+Uncaught TypeError: undefined is not a function pouchdb-3.0.6.min.js:7
+db._allDocs({include_docs: true}, function (err,response){console.log(response)});
+undefined
+Object {total_rows: 1, offset: undefined, rows: Array[1]}offset: undefinedrows: Array[1]0: Objectdoc: Objectid: "cabbage"key: "cabbage"value: Object__proto__: Objectlength: 1__proto__: Array[0]total_rows: 1__proto__: Object VM232:2
+db._allDocs({include_docs: true}, function (err,response){console.log(response.rows)});
+undefined
+[Object]0: Objectdoc: Objectid: "cabbage"key: "cabbage"value: Object__proto__: Objectlength: 1__proto__: Array[0] VM248:2
+db.sync('pouch',remoteDB.dsn);
+r {canceled: false, push: r, pull: r, _events: Object, then: function…}
+db._allDocs({include_docs: true}, function (err,response){console.log(response.rows)});
+undefined
+[Object]0: Objectdoc: Objectid: "cabbage"key: "cabbage"value: Object__proto__: Objectlength: 1__proto__: Array[0] VM307:2
+db.sync(remoteDB.dsn);
+r {canceled: false, push: r, pull: r, _events: Object, then: function…}
+GET http://elivero.cloudant.com/?_nonce=vQumLgbBZ5qrCOIG 404 (Object Not Found) elivero.cloudant.com/?_nonce=vQumLgbBZ5qrCOIG:1
+GET http://elivero.cloudant.com/?_nonce=sU7vyr1vRxFZhdks 404 (Object Not Found) elivero.cloudant.com/?_nonce=sU7vyr1vRxFZhdks:1
+GET http://elivero.cloudant.com/pouch/_local/sxPRxBSrSEByXZFEhim7Dw==?_nonce=Rko60M40qb0ra32g 404 (Object Not Found) elivero.cloudant.com/pouch/_local/sxPRxBSrSEByXZFEhim7Dw==?_nonce=Rko60M40qb0ra32g:1
+db.sync(remoteDB.dsn,'pouch');
+r {canceled: false, push: r, pull: r, _events: Object, then: function…}
+GET http://elivero.cloudant.com/?_nonce=iVknqQmLch2f0h7B 404 (Object Not Found) elivero.cloudant.com/?_nonce=iVknqQmLch2f0h7B:1
+GET http://elivero.cloudant.com/?_nonce=NBWU3kHYqPb8UiG7 404 (Object Not Found) elivero.cloudant.com/?_nonce=NBWU3kHYqPb8UiG7:1
