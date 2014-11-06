2ed266d4137a98407faef12b673223a32adafd26
Bojan Markovic <bmarkovic.79@gmail.com>
2014-11-06 12:56:17 +0100

    Update log1.js

--- log1.js
+++ log1.js
@@ -34,3 +34,67 @@ db.sync(remoteDB.dsn,'pouch');
 r {canceled: false, push: r, pull: r, _events: Object, then: function…}
 GET http://elivero.cloudant.com/?_nonce=iVknqQmLch2f0h7B 404 (Object Not Found) elivero.cloudant.com/?_nonce=iVknqQmLch2f0h7B:1
 GET http://elivero.cloudant.com/?_nonce=NBWU3kHYqPb8UiG7 404 (Object Not Found) elivero.cloudant.com/?_nonce=NBWU3kHYqPb8UiG7:1
+
+Remote DSN:  http://thoushoonlyconceencither:f6XftcvPtpb6hGT1YyRRS2hI@elivero.cloudant.com/pouch main.js:56
+GET blob:null/62b78ffd-b653-4624-8c99-fd5dd66328ae 404 (Not Found) pouchdb-3.0.6.min.js:8
+Ch-Ch-Changes main.js:50
+GET http://elivero.cloudant.com/?_nonce=yoiH75EvEl6fSDuG 404 (Object Not Found) elivero.cloudant.com/?_nonce=yoiH75EvEl6fSDuG:1
+db.sync('pouch', remoteDB.dsn);
+r {canceled: false, push: r, pull: r, _events: Object, then: function…}_events: Objectcanceled: truecatch: function (e){return m["catch"](e)}pull: rpush: rthen: function (e,t){return m.then(e,t)}__proto__: r
+db._allDocs;
+function (e,t){y(function(n,r){return n?t(n):0===e.limit?t(null,{total_rows:r,offset:e.skip,rows:[]}):void m(r,e,t)})} pouchdb-3.0.6.min.js:7
+console.log(db._al
+SyntaxError: Unexpected token }
+console.log(db._allDocs);
+function (e,t){y(function(n,r){return n?t(n):0===e.limit?t(null,{total_rows:r,offset:e.skip,rows:[]}):void m(r,e,t)})} VM230:2
+undefined
+db._allDocs(function (err,response){console.log(response)});
+undefined
+Uncaught TypeError: undefined is not a function pouchdb-3.0.6.min.js:7
+db._allDocs({include_docs: true}, function (err,response){console.log(response)});
+undefined
+Object {total_rows: 1, offset: undefined, rows: Array[1]}offset: undefinedrows: Array[1]0: Objectdoc: Objectid: "cabbage"key: "cabbage"value: Object__proto__: Objectlength: 1__proto__: Array[0]total_rows: 1__proto__: Object VM232:2
+db._allDocs({include_docs: true}, function (err,response){console.log(response.rows)});
+undefined
+[Object]0: Objectdoc: Objectid: "cabbage"key: "cabbage"value: Object__proto__: Objectlength: 1__proto__: Array[0] VM248:2
+db.sync('pouch',remoteDB.dsn);
+r {canceled: false, push: r, pull: r, _events: Object, then: function…}
+db._allDocs({include_docs: true}, function (err,response){console.log(response.rows)});
+undefined
+[Object]0: Objectdoc: Objectid: "cabbage"key: "cabbage"value: Object__proto__: Objectlength: 1__proto__: Array[0] VM307:2
+db.sync(remoteDB.dsn);
+r {canceled: false, push: r, pull: r, _events: Object, then: function…}
+GET http://elivero.cloudant.com/?_nonce=vQumLgbBZ5qrCOIG 404 (Object Not Found) elivero.cloudant.com/?_nonce=vQumLgbBZ5qrCOIG:1
+GET http://elivero.cloudant.com/?_nonce=sU7vyr1vRxFZhdks 404 (Object Not Found) elivero.cloudant.com/?_nonce=sU7vyr1vRxFZhdks:1
+GET http://elivero.cloudant.com/pouch/_local/sxPRxBSrSEByXZFEhim7Dw==?_nonce=Rko60M40qb0ra32g 404 (Object Not Found) elivero.cloudant.com/pouch/_local/sxPRxBSrSEByXZFEhim7Dw==?_nonce=Rko60M40qb0ra32g:1
+db.sync(remoteDB.dsn,'pouch');
+r {canceled: false, push: r, pull: r, _events: Object, then: function…}
+GET http://elivero.cloudant.com/?_nonce=iVknqQmLch2f0h7B 404 (Object Not Found) elivero.cloudant.com/?_nonce=iVknqQmLch2f0h7B:1
+GET http://elivero.cloudant.com/?_nonce=NBWU3kHYqPb8UiG7 404 (Object Not Found) elivero.cloudant.com/?_nonce=NBWU3kHYqPb8UiG7:1
+
+"Remote DSN: " "http://thoushoonlyconceencither:f6XftcvPtpb6hGT1YyRRS2hI@elivero.cloudant.com/pouch" main.js:56
+The character encoding of the HTML document was not declared. The document will render with garbled text in some browser configurations if the document contains characters from outside the US-ASCII range. The character encoding of the page must be declared in the document or in the transfer protocol. index.html
+"Ch-Ch-Changes" main.js:50
+db._allDocs({include_docs: true}, function (err,response){console.log(response.rows)});
+undefined
+Array [ Object ]
+db.replicate(remoteDB.dsn,'pouch');
+TypeError: db.replicate is not a function
+db.replicate(remoteDB.dsn,'pouch')
+TypeError: db.replicate is not a function
+db
+Object { auto_compaction: undefined, prefix: "_pouch_", _events: Object, _maxListeners: undefined, autoCompact: s/e.autoCompact(), taskqueue: Object, _adapter: "idb", _db_name: "pouch", adapter: "idb", replicate: Object, 17 more… }
+db.replicate.from(remoteDB.dsn)
+Object { _events: Object, _maxListeners: undefined, cancelled: false, then: r/e.then(), catch: r/e.catch() }
+db._allDocs({include_docs: true}, function (err,response){console.log(response.rows)});
+undefined
+Array [ Object, Object, Object ]
+db.put(_id:'cucumber',price:3,img:'img/cucumber.jpg')
+SyntaxError: missing ) after argument list
+db.put({_id:'cucumber',price:3,img:'img/cucumber.jpg'})
+Promise { cancel: [23]</</n.toPromise/</a.cancel() }
+db.replicate.to(remoteDB.dsn)
+Object { _events: Object, _maxListeners: undefined, cancelled: false, then: r/e.then(), catch: r/e.catch() }
+db._allDocs({include_docs: true}, function (err,response){console.log(response.rows)});
+undefined
+Array [ Object, Object, Object, Object ]
